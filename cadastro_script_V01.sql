e58d6ac23a3f6ec2d4bdc5ea0ae13bc382d5be50
CeciliaVieira <ceci.nv@gmail.com>
2021-07-27 16:53:18 -0300

    Adição de Chave Primária à tabela

--- cadastro_script_V01.sql
+++ cadastro_script_V01.sql
@@ -2,11 +2,15 @@ create database cadastro
 default character set utf8
 default collate utf8_general_ci;
 
+use cadastro;
+
 create table pessoas (
-nome varchar(30) NOT NULL,
-nascimento date,
-sexo enum('M', 'F'),
-peso decimal(5,2),
-altura decimal(3,2),
-nacionalidade varchar(20) default 'Brasil'
+`id` int NOT NULL auto_increment,
+`nome` varchar(30) NOT NULL,
+`nascimento` date,
+`sexo` enum('M', 'F'),
+`peso` decimal(5,2),
+`altura` decimal(3,2),
+`nacionalidade` varchar(20) default 'Brasil',
+primary key (id)
 ) default charset = utf8;
