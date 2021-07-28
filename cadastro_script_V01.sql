ef8dcfbbffe7841ea38fd364230f20dc81be3e69
CeciliaVieira <ceci.nv@gmail.com>
2021-07-28 16:12:34 -0300

    insert into ... values
    Inserção de dados na tabela com insert into

--- cadastro_script_V01.sql
+++ cadastro_script_V01.sql
@@ -14,3 +14,29 @@ create table pessoas (
 `nacionalidade` varchar(20) default 'Brasil',
 primary key (id)
 ) default charset = utf8;
+
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil');
+
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal');
+
+insert into pessoas values
+(default,'Creuza', '1920-10-25', 'F', '50.3', '1.65', default);
+
+insert into pessoas values
+(default,'Adalgiza', '1930-11-02', 'F', '63.2', '1.75', 'Irlanda');
+
+insert into pessoas
+(id, nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+(default, 'Ana', '1975-12-22', 'F', '52.3', '1.45', 'EUA'),
+(default, 'Pedro', '2000-07-15', 'M', '68.3', '1.65', 'Brasil'),
+(default, 'Cláudio', '1975-4-21', 'M', '99', '2.15', default);
+
+select * from pessoas;
+
