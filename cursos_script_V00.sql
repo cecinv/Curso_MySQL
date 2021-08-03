031df827ff30ba541fb21f06466f3b9ca32b9188
CeciliaVieira <ceci.nv@gmail.com>
2021-08-03 17:14:03 -0300

    Comando Select com order by e where

--- cursos_script_V00.sql
+++ cursos_script_V00.sql
@@ -60,3 +60,29 @@ where idcurso = '10';
 
 delete from cursos
 where idcurso >= '8';
+
+select * from cursos
+order by nome;  
+
+select ano, nome, carga from cursos
+order by ano, nome;
+
+select nome, carga from cursos
+where ano = '2016'
+order by nome;
+
+select nome, descricao, ano from cursos
+where ano <> 2016
+order by ano, nome;
+
+select nome, ano from cursos
+where ano between 2014 and 2016
+order by ano desc, nome;
+
+select nome, descricao, ano from cursos
+where ano in (2014, 2016)
+order by ano;
+
+select nome, carga, totalaulas from cursos
+where carga > 35 and totalaulas <= 30;
+
